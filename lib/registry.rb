1995b499fa7247542f70fd41bb701025c1145ec3
betrakiss <angelin.nedelchev@outlook.com>
2016-02-08 01:09:13 +0200

    Fixes bug with duplication fix not working, this logic needs to be taken elsewhere.

--- lib/registry.rb
+++ lib/registry.rb
@@ -11,7 +11,7 @@ class MovieRegistry
   end
 
   def add_user(name)
-    user = User.where(name: name).size > 0 || User.create(name: name)
+    user = User.where(name: name).empty? ? User.create(name: name) : User.where(name: name)
   end
 
   def add(id)
@@ -26,7 +26,7 @@ class MovieRegistry
   end
 
   def latest
-    Movie.all
+    Movie.all.take(5)
   end
 
   def is_series?(movie)
