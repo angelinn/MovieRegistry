6631515d815e0eff1534dc0e9815457e4e8b4aac
betrakiss <angelin.nedelchev@outlook.com>
2016-02-10 00:49:14 +0200

    Refactors episode manager.

--- lib/episode_manager.rb
+++ lib/episode_manager.rb
@@ -17,10 +17,10 @@ module Episodes
 
       @episodes
       current = @episodes.select { |e| e.season_number == last_season.to_s and e.number == last_number.to_s }.first
-      {
-        :title => @series.name,
-        :new_episodes => @episodes.select { |e| e.air_date and e.air_date > current.air_date and e.air_date < Date.parse(Time.new.to_s) }
-      }
+
+      Hash[:title => @series.name, :new_episodes =>
+          @episodes.select { |e| e.air_date and e.air_date > current.air_date and e.air_date < Date.parse(Time.new.to_s) }
+      ]
     end
 
     private
@@ -30,16 +30,6 @@ module Episodes
 
     def has_finished?(season, number)
       @episodes.sort { |a, b| a.season_number <=> b.season_number }.last.number == number.to_s
-    end
-  end
-
-  class Epi
-    attr_reader :name, :season, :number
-
-    def initialize(name, season, number)
-      @name = name
-      @season = season
-      @number = number
     end
   end
 end
