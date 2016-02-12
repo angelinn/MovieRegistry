afa304963b6e6847ac74ad5ab614b49de6a14e3c
betrakiss <angelin.nedelchev@outlook.com>
2016-02-12 14:37:49 +0200

    Moves sqlite connection.

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require_relative './config/environment'
 require_relative './lib/imdb_manager'
 require_relative './lib/registry'
 
+ActiveRecord::Base.establish_connection(
+  :adapter  => 'sqlite3',
+  :database => 'movie_registry.db'
+)
+
 not_found do
   status 404
   erb :'shared/oops', :layout => false
--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,3 @@ end
 Dir.glob('%s/models/*.rb' % Dir.pwd).each do |file|
   require_relative file
 end
-
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'sqlite3',
-  :database => 'movie_registry.db'
-)
